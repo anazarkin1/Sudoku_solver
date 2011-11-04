d7fd00a7a962512f48ccb4d11c3308caf2d33d0a
bearw@mail.ru <bearw@mail.ru>
2011-11-04 14:01:41 +1300

    rewrote readfile()

--- solver/Main.cs
+++ solver/Main.cs
@@ -16,60 +16,47 @@ namespace solver
 	}
 	class State
 	{
+		int[] range = new int[9]{1,2,3,4,5,6,7,8,9};
+		
 		private struct element
 		{
 		public 	List<int> candidates;
-			public int status;
+			public int status;	//0=set
+								//1 = unchecked(never touched it before)
+								//2 = unknown
 		}
 		
 		private 	int[,]field = new int[9,9];
 		private element[,] mask = new element[9,9];
 		public void readField (string filename)
 		{
-			using (StreamReader inputReader = new StreamReader(filename))
+			StreamReader streamReader = new StreamReader (filename);
+			string text = streamReader.ReadToEnd ();
+			string [] words = text.Split ();
+			int i = 0, j=0, k = 0;
+			while (k<words.Length)
 			{
-				int number = 0;
-				int i = 0, j = 0;
-				while ((number = inputReader.Read()) != -1)
+				if (j == 10)
 				{
-					if (j == 10)
-					{
-						j -= 10;
-						i++;
-					}
-					
-					field [i, j] = number;
-					int tmp = 1;
-					if (number != 0)
-					{
-						mask [i, j].status = 2;
-						
-						while (tmp != 10)
-						{
-							if (tmp == number)
-								continue;
-							
-							mask [i, j].candidates.Add (i);
-							tmp++;
-						}
-					
-					} else
-					{
-						mask [i, j].status = 0;
-						
-						while (tmp != 10)
-						{
-							mask [i, j].candidates.Add (i);
-							tmp++;
-						}
-
-					}
-					
-					
+					j = 0;
+					i++;
 				}
-						
+				Int32.TryParse (words [i], out field [i, j]);
+				if (field [i, j] == 0)
+				{	
+					mask [i, j].candidates.Add(1);
+					mask [i, j].status = 1;
+				} else
+					mask [i, j].status = 0;
+				
+				
+				k++;
+				
 			}
 		}
+		
+		
+		
 			
 		public void showField ()
 		{
